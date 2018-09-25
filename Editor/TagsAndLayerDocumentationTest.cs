b4e887272f61b683b9031214d1f0e1ef7e82ff77
Ghat Smith <ghatsmith@gmail.com>
2018-09-25 21:37:02 +0100

    Initial Commit

---
+++ Editor/TagsAndLayerDocumentationTest.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+
+public class TagsAndLayerDocumentationTest
+{
+    private TagsAndLayersDocumentation GetDocumentation()
+    {
+        TagsAndLayersDocumentation[] instances = GetAllInstances<TagsAndLayersDocumentation>();
+        if (instances == null || instances.Length != 1) return null;
+        else return instances[0];
+    }
+
+    [Test]
+    public void TestInstanceExistence()
+    {
+        TagsAndLayersDocumentation[] instances = GetAllInstances<TagsAndLayersDocumentation>();
+
+        Assert.IsTrue(instances != null && instances.Length > 0, "Instance of scriptable object TagsAndLayersDocumentation doesn't exists.");
+        Assert.IsTrue(instances.Length == 1, "Multiple instance of scriptable object TagsAndLayersDocumentation have been found.");
+    }
+
+    [Test]
+    public void TestTagsDocumentation()
+    {
+        TagsAndLayersDocumentation documentation = GetDocumentation();
+        Assert.IsNotNull(documentation, "Can't get instance of scriptable object. Refers to TestInstanceExistence for more details.");
+
+        SerializedObject serializedObject = new SerializedObject(documentation);
+        SerializedProperty tagsProperty = serializedObject.FindProperty("tagsDocumentation");
+
+        for (int i = 0; i < tagsProperty.arraySize; i++)
+        {
+            SerializedProperty tagProperty = tagsProperty.GetArrayElementAtIndex(i);
+            string name = tagProperty.FindPropertyRelative("name").stringValue;
+            string description = tagProperty.FindPropertyRelative("description").stringValue;
+            Assert.IsNotEmpty(description, string.Format("Tag {0} has empty description", name));
+        }
+    }
+
+    [Test]
+    public void TestSortingLayersDocumentation()
+    {
+        TagsAndLayersDocumentation documentation = GetDocumentation();
+        Assert.IsNotNull(documentation, "Can't get instance of scriptable object. Refers to TestInstanceExistence for more details.");
+
+        SerializedObject serializedObject = new SerializedObject(documentation);
+        SerializedProperty sortingLayersProperty = serializedObject.FindProperty("sortingLayersDocumentation");
+
+        for (int i = 0; i < sortingLayersProperty.arraySize; i++)
+        {
+            SerializedProperty sortingLayerProperty = sortingLayersProperty.GetArrayElementAtIndex(i);
+            string name = sortingLayerProperty.FindPropertyRelative("name").stringValue;
+            string description = sortingLayerProperty.FindPropertyRelative("description").stringValue;
+            Assert.IsNotEmpty(description, string.Format("Sorting layer {0} has empty description", name));
+        }
+    }
+
+    [Test]
+    public void TestLayersDocumentation()
+    {
+        TagsAndLayersDocumentation documentation = GetDocumentation();
+        Assert.IsNotNull(documentation, "Can't get instance of scriptable object. Refers to TestInstanceExistence for more details.");
+
+        SerializedObject serializedObject = new SerializedObject(documentation);
+        SerializedProperty layersProperty = serializedObject.FindProperty("layersDocumentation");
+
+        for (int i = 0; i < layersProperty.arraySize; i++)
+        {
+            SerializedProperty layerProperty = layersProperty.GetArrayElementAtIndex(i);
+            string name = layerProperty.FindPropertyRelative("name").stringValue;
+            string description = layerProperty.FindPropertyRelative("description").stringValue;
+            Assert.IsNotEmpty(description, string.Format("Layer {0} has empty description", name));
+        }
+    }
+
+    private static T[] GetAllInstances<T>() where T : ScriptableObject
+    {
+        string[] guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);  //FindAssets uses tags check documentation for more info
+        T[] a = new T[guids.Length];
+        for (int i = 0; i < guids.Length; i++)         //probably could get optimized 
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+        }
+
+        return a;
+    }
+}
---
+++ TagsAndLayersDocumentation.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+[CreateAssetMenu(fileName = "TagsAndLayersDocumentation", menuName = "Tools/TagsAndLayersDocumentation")]
+public class TagsAndLayersDocumentation : ScriptableObject
+{
+#if UNITY_EDITOR
+    [System.Serializable]
+    private struct Description
+    {
+        [HideInInspector] public string name;
+        [TextArea] public string description;
+    }
+
+
+    [SerializeField, Header("TAGS")] private Description[] tagsDocumentation;
+    [SerializeField, Space(20f), Header("SORTING LAYERS")] private Description[] sortingLayersDocumentation;
+    [SerializeField, Space(20f), Header("LAYERS")] private Description[] layersDocumentation;
+
+
+    void Reset()
+    {
+        tagsDocumentation = new Description[0];
+        sortingLayersDocumentation = new Description[0];
+        layersDocumentation = new Description[0];
+
+        OnValidate();
+
+        SetDefaultTagsDocumentation();
+        SetDefaultSortingLayersDocumentation();
+        SetDefaultLayersDocumentation();
+    }
+
+    private void SetDefaultTagsDocumentation()
+    {
+        for (int i = 0; i < tagsDocumentation.Length; i++)
+        {
+            switch (tagsDocumentation[i].name)
+            {
+                case "Untagged":
+                    tagsDocumentation[i].description = "Unity built in tag.";
+                    break;
+                case "Respawn":
+                    tagsDocumentation[i].description = "Unity built in tag.";
+                    break;
+                case "Finish":
+                    tagsDocumentation[i].description = "Unity built in tag.";
+                    break;
+                case "EditorOnly":
+                    tagsDocumentation[i].description = "Unity built in tag. An object tagged this will not appear in a build.";
+                    break;
+                case "MainCamera":
+                    tagsDocumentation[i].description = "Unity built in tag. An object tagged this will respond to Camera.main.";
+                    break;
+                case "Player":
+                    tagsDocumentation[i].description = "Unity built in tag.";
+                    break;
+                case "GameController":
+                    tagsDocumentation[i].description = "Unity built in tag.";
+                    break;
+                default:
+                    Debug.LogWarning("Need to add documentation for tag " + tagsDocumentation[i].name);
+                    break;
+            }
+        }
+
+    }
+
+    private void SetDefaultSortingLayersDocumentation()
+    {
+        for (int i = 0; i < sortingLayersDocumentation.Length; i++)
+        {
+            switch (sortingLayersDocumentation[i].name)
+            {
+                case "Default":
+                    sortingLayersDocumentation[i].description = "Unity built in sorting layer.";
+                    break;
+                default:
+                    Debug.LogWarning("Need to add documentation for sorting layer " + sortingLayersDocumentation[i].name);
+                    break;
+            }
+        }
+    }
+
+    private void SetDefaultLayersDocumentation()
+    {
+        for (int i = 0; i < layersDocumentation.Length; i++)
+        {
+            switch (layersDocumentation[i].name)
+            {
+                case "Default":
+                    layersDocumentation[i].description = "Unity built in layer.";
+                    break;
+                case "TransparentFX":
+                    layersDocumentation[i].description = "Unity built in layer. Lens flares will show through the collider of an object on this layer.";
+                    break;
+                case "Ignore Raycast":
+                    layersDocumentation[i].description = "Unity built in layer. An object on this layer will be ignored by raycasts (including OnMouseOver, etc.).";
+                    break;
+                case "Water":
+                    layersDocumentation[i].description = "Unity built in layer.";
+                    break;
+                case "UI":
+                    layersDocumentation[i].description = "Unity built in layer.";
+                    break;
+                default:
+                    Debug.LogWarning("Need to add documentation for layer " + layersDocumentation[i].name);
+                    break;
+            }
+        }
+    }
+
+    public void OnValidate()
+    {
+        string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+        if (tagsDocumentation.Length != tags.Length) System.Array.Resize(ref tagsDocumentation, tags.Length);
+
+        for (int i = 0; i < tags.Length; i++)
+        {
+            tagsDocumentation[i].name = tags[i];
+        }
+
+        SortingLayer[] sortingLayers = SortingLayer.layers;
+        if (sortingLayersDocumentation.Length != sortingLayers.Length) System.Array.Resize(ref sortingLayersDocumentation, sortingLayers.Length);
+
+        for (int i = 0; i < sortingLayers.Length; i++)
+        {
+            sortingLayersDocumentation[i].name = sortingLayers[i].name;
+        }
+
+        string[] layers = UnityEditorInternal.InternalEditorUtility.layers;
+        if (layersDocumentation.Length != layers.Length) System.Array.Resize(ref layersDocumentation, layers.Length);
+
+        for (int i = 0; i < layersDocumentation.Length; i++)
+        {
+            layersDocumentation[i].name = layers[i];
+        }
+    }
+#endif
+}
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TagsAndLayersDocumentation))]
+public class TagsAndLayersDocumentationEditor : Editor
+{
+    void OnEnable()
+    {
+        // Force calling OnValidate method when displaying inspector
+        (target as TagsAndLayersDocumentation).OnValidate();
+    }
+}
+#endif
